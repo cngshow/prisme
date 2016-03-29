375c9d93fba6e56ffff35d8d7bfe578c14d25854
cshupp <cshupp@gmail.com>
2016-03-29 13:54:12 -0400

    Data Model changes for a job

--- db/migrate/20160329150821_create_prisme_jobs.rb
+++ db/migrate/20160329150821_create_prisme_jobs.rb
@@ -1,8 +1,8 @@
 class CreatePrismeJobs < ActiveRecord::Migration
   def self.up
-    create_table :prisme_jobs, force: true do |table|
-      table.string :job_id, :null => false            #the assigned global_id guid
-      table.string :job_name, :null => false          #the name of the job
+    create_table :prisme_jobs,{force: true} do |table|
+      table.string :job_id, :null => false            # the assigned global_id guid
+      table.string :job_name, :null => false          # the name of the job
       table.string :status, :null => false            # not_queued, queued, running, completed, failed
       table.string :queue, :null => false             # The name of the queue this job is in
       table.datetime :scheduled_at, :null => false    # When the job is scheduled to run.
@@ -10,7 +10,7 @@ class CreatePrismeJobs < ActiveRecord::Migration
       table.datetime :started_at                      # When the job was started
       table.datetime :completed_at                    # When the job was completed
       table.text :last_error                          # reason for last failure (See Note below)
-      table.string :user                              #the user requesting the job
+      table.string :user                              # the user requesting the job
       table.timestamps null: true
     end
 
@@ -20,6 +20,7 @@ class CreatePrismeJobs < ActiveRecord::Migration
     add_index :prisme_jobs, [:job_name], name: "prisme_job_job_name"
     add_index :prisme_jobs, [:completed_at], name: "prisme_job_completed_at"
     add_index :prisme_jobs, [:scheduled_at], name: "prisme_job_scheduled_at"
+    add_index :prisme_jobs, [:job_id], name: "prisme_job_job_id", :unique => true
   end
 
   def self.down
