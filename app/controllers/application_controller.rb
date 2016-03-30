69657757902647099e282d0f464940e84d4e7496
cshupp <cshupp@gmail.com>
2016-03-30 13:15:07 -0400

    Adding gon into prisme so a status hash is available for lookup in the land of JavaScript.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+  before_action :setup_gon
   rescue_from Exception, :with => :internal_error
 
   def internal_error(e)
@@ -10,6 +10,10 @@ class ApplicationController < ActionController::Base
     $log.error request.fullpath
     $log.error(e.backtrace.join("\n"))
     raise e
+  end
+
+  def setup_gon
+    gon.job_status_constants = PrismeJobConstants::Status::STATUS_HASH.invert
   end
 
 end
