3b5dc036e62162b9ea474c8649bf7dd02fbcc013
cshupp <cshupp@gmail.com>
2016-03-30 13:13:39 -0400

    Modifying initializer so that it doesn't run DB saves during a migration or rollback.

--- config/initializers/sucker_punch.rb
+++ config/initializers/sucker_punch.rb
@@ -32,21 +32,23 @@ module ActiveJob
           time = java.util.Date.new(timestamp * 1000)
           timer = JTimer.new(job.to_s)
           timer_task = TimerTask.new
-          timer_task.set_runnable(->{JobWrapper.new.async.perform job.serialize })
-          timer.java_send(:schedule,[JTimerTask.java_class, java.util.Date.java_class],timer_task, time)
+          timer_task.set_runnable(-> { JobWrapper.new.async.perform job.serialize })
+          timer.java_send(:schedule, [JTimerTask.java_class, java.util.Date.java_class], timer_task, time)
         end
       end
     end
   end
 end
 
-# update any uncompleted jobs that are in the PrismeJobs table to failure status
-params = [PrismeJobConstants::Status::COMPLETED,
-          PrismeJobConstants::Status::FAILED,
-]
 
-update_hash = {status: PrismeJobConstants::Status::FAILED, last_error: 'System Failure - Incompleted job update to Failed in initializer!'}
-PrismeJob.where(['status != ? and status != ?', *params]).update_all(update_hash)
+unless defined?(is_running_migration_or_rollback?) && is_running_migration_or_rollback?
+# update any uncompleted jobs that are in the PrismeJobs table to failure status
+  params = [PrismeJobConstants::Status::STATUS_HASH[:COMPLETED],
+            PrismeJobConstants::Status::STATUS_HASH[:FAILED],]
+  update_hash = {status: PrismeJobConstants::Status::STATUS_HASH[:FAILED], last_error: 'System Failure - Incompleted job update to Failed in initializer!'}
+  PrismeJob.where(['status != ? and status != ?', *params]).update_all(update_hash)
 
 # schedule the CleanJobQueueJob to run soon
-CleanJobQueueJob.perform_later
+  CleanJobQueueJob.perform_later
+end
+
