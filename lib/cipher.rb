a9fb046ba67038f4c6ae4baf3ba3e53cd86dfbaa
cshupp <cshupp@gmail.com>
2016-04-12 11:52:47 -0400

    Encryption repairs

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -3,14 +3,19 @@ class CipherSupport
 
   def encrypt(unencrypted_string:)
     init
+    $log.info("Encrypt string " + unencrypted_string)
     @encrypt.update(unencrypted_string)
     @encrypt.final.bytes.to_s
   end
 
   def decrypt(encrypted_string:)
     init
+    $log.info("Decrypt string " + encrypted_string)
     b = eval encrypted_string
-    @decrypt.update( b.map(&:chr).join)
+    $log.info("Decrypt array " + b.inspect)
+    decrypt_me = b.map(&:chr).join
+    $log.info("dddd " + decrypt_me)
+    @decrypt.update( decrypt_me)
     @decrypt.final
   end
 
@@ -19,7 +24,7 @@ class CipherSupport
     #128 bit AES Cipher Block Chaining encryption
     @encrypt = OpenSSL::Cipher::AES.new(128, :CBC)
     @decrypt = OpenSSL::Cipher::AES.new(128, :CBC)
-    secret = Rails.application.secrets.secret_key_base
+    secret = Rails.application.secrets.secret_key_cipher_support
     @encrypt.key = secret
     @decrypt.key = secret
     @encrypt.encrypt
