9d22791ce68c9e5f7f5fe74f23d02c4513dad02a
cshupp <cshupp@gmail.com>
2016-04-18 14:00:33 -0400

    Forcing authenticator to use custom views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  devise_scope :user do get '/users/sign_out' => 'devise/sessions#destroy' end
-  devise_scope :user do get '/users/sign_in' => 'devise/sessions#create' end
-  devise_scope :user do get '/users/sign_up' => 'devise/sessions#new' end
 
   #
   # devise_for :users, :controllers => { :registrations => "registrations", :sessions => "sessions" }
