2b5ed131236e7e290b31a7952294b9d1e3192ccd
gpbowman-git <gpbowman.git@gmail.com>
2016-04-19 08:17:21 -0400

    modified polling javascript

--- app/assets/javascripts/polling.js
+++ app/assets/javascripts/polling.js
@@ -1,18 +1,40 @@
-var $polling = {};
+var polling = (function () {
+    // privates
+    var registrations = {};
 
-function register_polling(controller, interval) {
-    console.log("****************************** registering polling for " + controller + " for interval " + interval.toString());
-    $polling[controller] = interval;
-}
-
-function checkPolling(controller) {
-    if (!$.isEmptyObject($polling)) {
-        $.each($polling, function (key, interval) {
-            if (controller !== key) {
-                console.log('***************** stop polling for interval ' + interval.toString());
-                clearInterval(interval);
-                delete $polling['' + key];
-            }
-        });
+    function isPolling(controller) {
+        return registrations[controller];
     }
-}
+
+    function register(controller, callback, interval_seconds) {
+        if (registrations[controller]) {
+            return false;
+        }
+        registrations[controller] = setInterval(callback, interval_seconds);
+    }
+
+    function checkPolling(controller) {
+        if (!$.isEmptyObject(registrations)) {
+            $.each(registrations, function (key, interval) {
+                if (controller !== key) {
+                    console.log('***************** stop polling for interval ' + interval.toString());
+                    clearInterval(interval);
+                    delete registrations['' + key];
+                }
+            });
+        }
+    }
+
+    // Return an object exposed to the public
+    return {
+        // public methods
+        // register a controller and its
+        registerController: register,
+
+        // call checkPolling to turn off any polling not associated with the controller key passed
+        checkPolling: checkPolling,
+
+        // Public alias to a private function
+        isPolling: isPolling
+    };
+})();
