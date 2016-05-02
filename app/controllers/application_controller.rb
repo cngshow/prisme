ddb6cfa3e490131ed95898543fed254ae577da85
cshupp <cshupp@gmail.com>
2016-05-02 12:09:37 -0400

    Needed to add gon.routes api.  Converted existing javascript to use it.  Without this change the javascript will not work on tomcat.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,7 +31,9 @@ class ApplicationController < ActionController::Base
 
   def setup_gon
     gon.job_status_constants = PrismeJobConstants::Status::STATUS_HASH.invert
+    setup_routes
   end
+
 
   def auth_registered
     authorize :navigation, :registered?
@@ -39,6 +41,37 @@ class ApplicationController < ActionController::Base
 
   def auth_admin
     authorize :navigation, :admin?
+  end
+
+  private
+  def setup_routes
+    routes = Rails.application.routes.named_routes.helpers.to_a
+    @@routes_hash ||= {}
+    if(@@routes_hash.empty?)
+      routes.each do |route|
+        begin
+          @@routes_hash[route.to_s] = self.send(route)
+        rescue ActionController::UrlGenerationError => ex
+          if (ex.message =~ /missing required keys: \[(.*?)\]/)
+            keys = $1
+            keys = keys.split(',')
+            keys.map! do |e|
+              e.gsub!(':', '')
+              e.strip
+            end
+            required_keys_hash = {}
+            keys.each do |key|
+              required_keys_hash[key.to_sym] = ':' + key.to_s
+            end
+            @@routes_hash[route.to_s] = self.send(route, required_keys_hash)
+          else
+            raise ex
+          end
+        end
+      end
+    end
+    $log.debug('routes hash passed to javascript is ' + @@routes_hash.to_s)
+    gon.routes = @@routes_hash
   end
 
 end
