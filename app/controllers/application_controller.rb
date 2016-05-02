74fc43e903284cee5f7cf2aa7e54bcb2de83cb0a
cshupp <cshupp@gmail.com>
2016-05-02 12:19:03 -0400

    Needed to add gon.routes api.  Converted existing javascript to use it.  Without this change the javascript will not work on tomcat.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 require './app/policies/navigation_policy'
+require './lib/rails_common/util/controller_helpers'
 
 class ApplicationController < ActionController::Base
   include ApplicationHelper
   include Pundit
+  include CommonController
+
   after_action :verify_authorized, unless: :devise_controller?
 
   # Prevent CSRF attacks by raising an exception.
@@ -41,37 +44,6 @@ class ApplicationController < ActionController::Base
 
   def auth_admin
     authorize :navigation, :admin?
-  end
-
-  private
-  def setup_routes
-    routes = Rails.application.routes.named_routes.helpers.to_a
-    @@routes_hash ||= {}
-    if(@@routes_hash.empty?)
-      routes.each do |route|
-        begin
-          @@routes_hash[route.to_s] = self.send(route)
-        rescue ActionController::UrlGenerationError => ex
-          if (ex.message =~ /missing required keys: \[(.*?)\]/)
-            keys = $1
-            keys = keys.split(',')
-            keys.map! do |e|
-              e.gsub!(':', '')
-              e.strip
-            end
-            required_keys_hash = {}
-            keys.each do |key|
-              required_keys_hash[key.to_sym] = ':' + key.to_s
-            end
-            @@routes_hash[route.to_s] = self.send(route, required_keys_hash)
-          else
-            raise ex
-          end
-        end
-      end
-    end
-    $log.debug('routes hash passed to javascript is ' + @@routes_hash.to_s)
-    gon.routes = @@routes_hash
   end
 
 end
