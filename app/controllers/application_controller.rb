e764d74bd4a6ef4bd392b3f1e9315c2afacb7f39
cshupp <cshupp@gmail.com>
2016-05-10 12:43:00 -0400

    Making an error page show up if Git is un-reachable. Ensuring the error is logged to our logs.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,22 +14,24 @@ class ApplicationController < ActionController::Base
   before_action :setup_gon
   rescue_from Exception, :with => :internal_error
 
-  def internal_error(e)
-    $log.error(e.message)
+  def internal_error(exception)
+    $log.error(exception.message)
     $log.error request.fullpath
-    $log.error(e.backtrace.join("\n"))
+    $log.error(exception.backtrace.join("\n"))
 
-    if (e.is_a?(Pundit::AuthorizationNotPerformedError) || e.is_a?(Pundit::NotAuthorizedError))
-      #When warbler warbles it puts static html one level above Rails Root!
-      render :file => (trinidad? ? 'public/not_authorized.html' :  "#{Rails.root}/../not_authorized.html")
-      return
-    elsif e.is_a? Faraday::ConnectionFailed
-      # thrown if Nexus is down.
-      render :file => (trinidad? ? 'public/nexus_not_available.html' :  "#{Rails.root}/../nexus_not_available.html")
-      return
+    case exception
+      when Pundit::AuthorizationNotPerformedError
+      when Pundit::NotAuthorizedError
+        render :file => (trinidad? ? 'public/not_authorized.html' : "#{Rails.root}/../not_authorized.html")
+        return
+      when Faraday::ConnectionFailed
+        render :file => (trinidad? ? 'public/nexus_not_available.html' : "#{Rails.root}/../nexus_not_available.html")
+        return
+      when JIsaacGit::GitFailureException
+        render :file => (trinidad? ? 'public/git_not_available.html' : "#{Rails.root}/../git_not_available.html")
+        return
     end
-
-    raise e
+    raise exception
   end
 
   def setup_gon
@@ -50,7 +52,7 @@ class ApplicationController < ActionController::Base
     artifactory_configured = Service.service_exists? PrismeService::NEXUS
     build_server_configured = Service.service_exists? PrismeService::JENKINS
     application_server_configured = Service.service_exists? PrismeService::TOMCAT
-    render :file => (trinidad? ? 'public/not_configured.html' :  "#{Rails.root}/../not_configured.html")  unless (application_server_configured && artifactory_configured  && build_server_configured)
+    render :file => (trinidad? ? 'public/not_configured.html' : "#{Rails.root}/../not_configured.html") unless (application_server_configured && artifactory_configured && build_server_configured)
     return
   end
 
