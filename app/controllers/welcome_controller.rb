bbac42d6851d755000df23d217842008bad251ae
gpbowman-git <gpbowman.git@gmail.com>
2016-05-12 13:03:34 -0400

    refactored code to include the link to the tomcat application in the deployments hash

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -10,27 +10,9 @@ class WelcomeController < ApplicationController
     $log.debug(current_user.email) unless current_user.nil?
     $log.debug(current_user.to_s) if current_user.nil?
 
-    # retrieve tomcat services
-    tomcats = Service.get_application_servers
-    tomcat_deployments = {}
-
-    # iterate and make faraday call to get the text/list
-    tomcats.each do |tomcat|
-      # get the properties hash for the tomcat service
-      service_props = tomcat.properties_hash
-      url = service_props[PrismeService::CARGO_REMOTE_URL]
-      username = service_props[PrismeService::CARGO_REMOTE_USERNAME]
-      password = service_props[PrismeService::CARGO_REMOTE_PASSWORD]
-      data_hash = nil
-
-      begin
-        data_hash = get_deployments(url: url, username: username, pwd: password)
-        tomcat_deployments[{url: url, service_name: tomcat.name}] = data_hash
-        @deployments = format_deployments_table_data(tomcat_deployments)
-      rescue
-      end
-      $log.debug("tomcat_deployments are #{@deployments}")
-    end
+    # get tomcat deployments
+    tomcat_deployments = tomcat_server_deployments
+    @deployments = format_deployments_table_data(tomcat_deployments)
   end
 
   #delete this before single sign on
@@ -50,12 +32,6 @@ class WelcomeController < ApplicationController
     ret = []
 
     tomcat_deployments.keys.each do |appserver|
-      url = appserver[:url]
-      uri = URI.parse(url)
-      scheme = uri.scheme
-      host = uri.host
-      port = uri.port
-      link = "#{scheme}://#{host}:#{port}"
       service_name = appserver[:service_name]
       current_row = {service_name: service_name, available: false, rows: []}
 
@@ -63,13 +39,10 @@ class WelcomeController < ApplicationController
       tomcat_deployments[appserver].each_pair do |war, d|
         current_row[:available] = true
         next if war.eql? :failed
-        row_click_url = link.clone << d[:context]
-        war_name = war
+
         # filter to only display isaac and rails war files
         next unless war =~ /^(isaac|rails).*/
-        state = d[:state]
-        session_count = d[:session_count].to_s
-        current_row[:rows] << {war_name: war_name, state: state, session_count: session_count, link: row_click_url}
+        current_row[:rows] << {war_name: war, state: d[:state], session_count: d[:session_count].to_s, link: d[:link]}
       end
       ret << current_row
     end
