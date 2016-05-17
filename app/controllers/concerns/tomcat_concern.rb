2bb5087b9dc1de44c8dbb46b9249c6306d79ba33
gpbowman-git <gpbowman.git@gmail.com>
2016-05-17 17:55:02 -0400

    formatting and wiring of the deployments page

--- app/controllers/concerns/tomcat_concern.rb
+++ app/controllers/concerns/tomcat_concern.rb
@@ -2,24 +2,66 @@ require 'faraday'
 require 'uri'
 
 module TomcatConcern
+  VALID_ACTIONS = [:start, :stop, :undeploy]
 
-# change_state(url: "http://localhost:8080/",username: "devtest",pwd: "devtest", context: "rails_komet_b", path: 'start')
-  def change_state(url:, username:, pwd:, context:, path:)
-    context = '/' + context unless context[0].eql?('/')
+  # change_state(url: "http://localhost:8080/",username: "devtest",pwd: "devtest", context: "rails_komet_b", path: 'start')
+  def change_state(tomcat_service_id:, context:, action:)
+    unless (VALID_ACTIONS.include?(action.to_sym))
+      $log.error("Invalid action, #{action}, passed into change_state method. Valid actions are: #{VALID_ACTIONS.to_s}.")
+      raise StandardError.new("Invalid action, #{action}, passed into change_state method. Valid actions are: #{VALID_ACTIONS.to_s}.")
+    end
 
-    conn = get_connection(url: url, username: username, pwd: pwd)
     begin
-      response = conn.get("/manager/text/#{path}", {path: path})
+      context = '/' + context unless context[0].eql?('/')
+      conn = get_connection(service_or_id: tomcat_service_id)
+
+      # this should not get hit, however, raise an exception if conn is nil
+      raise "Tomcat Connection Error. Unable to get a Tomcat Connection for #{tomcat_service_id.to_s}" if conn.nil?
+
+      # make the rest call to change the state of the tomcat deployment based on the action (start, stop, undeploy) and the context (rails_komet, etc.)
+      response = conn.get("/manager/text/#{action}", {path: context})
     rescue Faraday::ConnectionFailed => ex
       $log.warn("Tomcat is unreachable! #{ex.message}")
+      return {failed: ex.message}
+    rescue => ex
+      $log.warn("Unexpected Exception: #{ex.message}")
       return {failed: ex.message}
     end
     response.body
   end
 
-  def get_deployments(url:, username:, pwd:)
+  private
+  def get_connection(service_or_id:)
+    conn = nil
 
-    conn = get_connection(url: url, username: username, pwd: pwd)
+    if (service_or_id.is_a? Service)
+      tomcat_service = service_or_id
+    else
+      tomcat_service = Service.find_by!(id: service_or_id)
+    end
+
+    # verify that we have a tomcat service
+    if (tomcat_service.service_type.eql?(PrismeService::TOMCAT))
+      service_props = tomcat_service.properties_hash
+      url = service_props[PrismeService::CARGO_REMOTE_URL]
+      username = service_props[PrismeService::CARGO_REMOTE_USERNAME]
+      pwd = service_props[PrismeService::CARGO_REMOTE_PASSWORD]
+
+      conn = Faraday.new(url: url) do |faraday|
+        faraday.request :url_encoded # form-encode POST params
+        faraday.use Faraday::Response::Logger, $log
+        faraday.adapter :net_http # make requests with Net::HTTP
+        faraday.basic_auth(username, pwd)
+      end
+    else
+      raise StandardError.new('Invalid tomcat service passed to TomcatConcern.get_deployments!')
+    end
+    conn
+  end
+
+  def get_deployments(tomcat_service:)
+    conn = get_connection(service_or_id: tomcat_service)
+
     # get the list of deployed applications
     response = nil
 
@@ -37,17 +79,16 @@ module TomcatConcern
       data.shift # remove the OK line
       ret_hash = {}
 
-
       data.each do |line|
         vars = line.split(':')
-        war = vars[3] #war is 'isaac_rest' or rails_komet_b or similiar with version
+        war = vars[3] #war is 'isaac_rest' or rails_komet_b or similar with version
         # filter to only display isaac and rails war files
         next unless war =~ /^(isaac|rails_k).*/
         ret_hash[war] = {}
         ret_hash[war][:context] = vars[0]
         ret_hash[war][:state] = vars[1]
         ret_hash[war][:session_count] = vars[2]
-        ret_hash[war][:version] = get_version(war: war, context: vars[0], url: url, username: username, pwd: pwd)
+        ret_hash[war][:version] = get_version(war: war, context: vars[0], tomcat_service: tomcat_service)
       end
       $log.debug("Returning #{ret_hash}")
       #      {"ROOT"=>{:context=>"/", :state=>"running", :session_count=>"0"}, "isaac-rest"=>{:context=>"/isaac-rest", :state=>"running
@@ -59,9 +100,8 @@ module TomcatConcern
     end
   end
 
-  def get_version(war:, context:, url:, username:, pwd:)
-    base_url = URI(url).base_url
-    conn = get_connection(url: base_url, username: username, pwd: pwd)
+  def get_version(war:, context:, tomcat_service:)
+    conn = get_connection(service_or_id: tomcat_service)
     path = ''
     version = "UNKNOWN"
     response = nil
@@ -83,7 +123,6 @@ module TomcatConcern
     begin
       json = JSON.parse body
     rescue => ex
-      $log.warn("Invalid JSON from " + base_url + path)
       json['version'] = "INVALID_JSON"
       json['restVersion'] = "INVALID_JSON"
     end
@@ -103,18 +142,13 @@ module TomcatConcern
 
     # iterate and make faraday call to get the text/list
     tomcats.each do |tomcat|
-      # get the properties hash for the tomcat service
-      service_props = tomcat.properties_hash
-      url = service_props[PrismeService::CARGO_REMOTE_URL]
-      username = service_props[PrismeService::CARGO_REMOTE_USERNAME]
-      password = service_props[PrismeService::CARGO_REMOTE_PASSWORD]
-      data_hash = get_deployments(url: url, username: username, pwd: password)
+      data_hash = get_deployments(tomcat_service: tomcat)
 
       # build the url to the tomcat server
-      uri = URI.parse(url)
-      scheme = uri.scheme
-      host = uri.host
-      port = uri.port
+      url = URI.parse(tomcat.properties_hash[PrismeService::CARGO_REMOTE_URL])
+      scheme = url.scheme
+      host = url.host
+      port = url.port
 
       # build the link for each deployment
       if data_hash.has_key?(:failed)
@@ -126,22 +160,9 @@ module TomcatConcern
           d.last[:link] = link
         end
 
-        tomcat_deployments[{url: url, service_name: tomcat.name}] = data_hash
+        tomcat_deployments[{url: url, service_name: tomcat.name, service_id: tomcat.id}] = data_hash
       end
     end
     tomcat_deployments
   end
-
-  private
-
-  def get_connection(url:, username:, pwd:)
-    conn = Faraday.new(url: url) do |faraday|
-      faraday.request :url_encoded # form-encode POST params
-      faraday.use Faraday::Response::Logger, $log
-      faraday.adapter :net_http # make requests with Net::HTTP
-      faraday.basic_auth(username, pwd)
-    end
-    conn
-  end
-
 end
--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,22 @@
 module WelcomeHelper
+  def action_button_bar(deployment, service_id)
+    ret = %{
+      <div style="display: inline-block">
+      <a class="btn btn-default" role="button" onclick="tomcat_app(SERVICE_ID, 'START_STOP_ACTION', 'DEPLOYMENT');"><i class="fa START_STOP_ICON" aria-hidden="true"></i>&nbsp;START_STOP_LABEL</i></a>
+      <a class="btn btn-default" role="button" onclick="tomcat_app(SERVICE_ID, 'undeploy', 'DEPLOYMENT');"><i class="fa fa-trash-o" aria-hidden="true"></i>&nbsp;Undeploy</i></a>
+      </div>
+    }
+
+    # check the deployment state to see if it is running, etc.
+    replacement_strings = %w(start fa-play-circle Start)
+
+    if (deployment[:state].eql?('running'))
+      replacement_strings = %w(stop fa-stop-circle-o Stop)
+    end
+
+    ret.gsub!('START_STOP_ACTION', replacement_strings[0]).gsub!('START_STOP_ICON', replacement_strings[1]).gsub!('START_STOP_LABEL', replacement_strings[2])
+    ret.gsub!('DEPLOYMENT', deployment[:war_name])
+    ret.gsub!('SERVICE_ID', service_id.to_s)
+    ret.html_safe
+  end
 end
