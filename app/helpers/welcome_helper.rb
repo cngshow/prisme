637aca361238ba33ebd76318d90ef10c8500fae9
cshupp <cshupp@gmail.com>
2016-05-20 09:51:57 -0400

    Lucky me.  My particular setup happens to break the new spinning gears that give the users feedback that a start/stop/deploy is occuring.

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -2,8 +2,8 @@ module WelcomeHelper
   def action_button_bar(deployment, service_id)
     ret = %{
 <div style="display: inline-block">
-<a id="SERVICE_ID-START_STOP_ACTION-DEPLOYMENT" class="btn btn-default" role="button" onclick="tomcat_app(this, SERVICE_ID, 'START_STOP_ACTION', 'DEPLOYMENT');"><i class="fa START_STOP_ICON fa-fw" aria-hidden="true"></i>&nbsp;START_STOP_LABEL</a>
-<a id="SERVICE_ID-undeploy-DEPLOYMENT" class="btn btn-default" role="button" onclick="tomcat_app(this, SERVICE_ID, 'undeploy', 'DEPLOYMENT');"><i class="fa fa-trash-o fa-fw" aria-hidden="true"></i>&nbsp;Undeploy</a>
+<a id="SERVICE_ID-START_STOP_ACTION-DEPLOYMENT_ID" class="btn btn-default" role="button" onclick="tomcat_app(this, SERVICE_ID, 'START_STOP_ACTION', 'DEPLOYMENT');"><i class="fa START_STOP_ICON fa-fw" aria-hidden="true"></i>&nbsp;START_STOP_LABEL</a>
+<a id="SERVICE_ID-undeploy-DEPLOYMENT_ID" class="btn btn-default" role="button" onclick="tomcat_app(this, SERVICE_ID, 'undeploy', 'DEPLOYMENT');"><i class="fa fa-trash-o fa-fw" aria-hidden="true"></i>&nbsp;Undeploy</a>
 </div>
 }
     # check the deployment state to see if it is running, etc.
@@ -14,6 +14,7 @@ module WelcomeHelper
     end
 
     ret.gsub!('START_STOP_ACTION', replacement_strings[0]).gsub!('START_STOP_ICON', replacement_strings[1]).gsub!('START_STOP_LABEL', replacement_strings[2])
+    ret.gsub!('DEPLOYMENT_ID', deployment[:war_name].gsub('.','_'))
     ret.gsub!('DEPLOYMENT', deployment[:war_name])
     ret.gsub!('SERVICE_ID', service_id.to_s)
     ret.html_safe
