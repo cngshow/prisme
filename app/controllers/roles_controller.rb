ab87e380e1e30b63d64344ee1e55eb6b08975512
cshupp <cshupp@gmail.com>
2016-06-01 12:07:09 -0400

    Adding in roles rest API into PRISME. Adding in basic SSL support for trinidad. Adding a quick and dirty hook for requiring roles be dished up via SSL.

---
+++ app/controllers/roles_controller.rb
@@ -0,0 +1,30 @@
+class RolesController < ApplicationController
+
+  skip_after_action :verify_authorized
+  skip_before_action :verify_authenticity_token
+  force_ssl if: :ssl_configured?
+
+
+  def get_roles
+    @user_id = params[:id]
+    $log.debug("About to fetch the roles for ID #{@user_id}")
+    @roles_array = []
+    user = User.find_by(email: @user_id)
+    unless (user.nil?)
+      user.roles.each do |role|
+        @roles_array << role
+      end
+    end
+    respond_to do |format|
+      format.html # get_roles.html.erb
+      format.json { render :json => @roles_array }
+    end
+  end
+
+  private
+
+  def ssl_configured?
+    false
+  end
+
+end
