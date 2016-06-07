04e6f3913bc7ff3f3de2149fa72b373360cb8b18
gpbowman-git <gpbowman.git@gmail.com>
2016-06-07 14:16:26 -0400

    modified users to always set readonly role; removed administrator column from devise user table (deleted migration); added logic to not allow the user to deselect the read only role in the GUI; added constraint on name for roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ActiveRecord::Base
+  before_save :ensure_read_only
+
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  private
+  def ensure_read_only
+    self.add_role(Roles::READ_ONLY)
+  end
 end
