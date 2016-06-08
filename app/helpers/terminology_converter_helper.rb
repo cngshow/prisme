5fe47d65a731bb105516d8dbbab363eb563ad9ee
gpbowman-git <gpbowman.git@gmail.com>
2016-06-08 09:52:24 -0400

    refactored code moving loading of dropdowns into TerminologyConverterHelper; added code for loading additional source and ibdf dependencies based on the selected source;

--- app/helpers/terminology_converter_helper.rb
+++ app/helpers/terminology_converter_helper.rb
@@ -1,2 +1,30 @@
 module TerminologyConverterHelper
+  include NexusConcern
+
+  def load_drop_down(nexus_params: nexus_params)
+    url_string = '/nexus/service/local/lucene/search'
+    options = []
+    response = get_nexus_connection.get(url_string, nexus_params)
+    json = nil
+
+    begin
+      json = JSON.parse(response.body)
+    rescue JSON::ParserError => ex
+      if (response.status.eql?(200))
+        return response.body
+      end
+    end
+
+    if (json && json.has_key?('data'))
+      json['data'].each do |a|
+        options << TermConvertOption.new(a['groupId'], a['artifactId'], a['version']) # todo CLASSIFIER for IBDFs? ask Dan
+      end
+
+      options.sort_by!(&:option_key).reverse!# the reverse will make the most recent versions on top
+    else
+      $log.debug("EMPTY nexus repository search for #{url_string}&#{nexus_params}")
+    end
+
+    options
+  end
 end
