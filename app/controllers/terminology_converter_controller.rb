7276152af459819dbdf14d69310d51f52cc086a6
gpbowman-git <gpbowman.git@gmail.com>
2016-07-05 17:19:38 -0400

    added code to use jquery validation on the terminology converter page to validate dynamic fields based on the regex expression returned

--- app/controllers/terminology_converter_controller.rb
+++ app/controllers/terminology_converter_controller.rb
@@ -183,11 +183,10 @@ class TerminologyConverterController < ApplicationController
 =end
 
   def ajax_converter_version_change
-
     converter_options = fetch_converter_options
     json = converter_options.map do |co|
       {display_name: co.display_name, description: co.description, internal_name: co.internal_name, allow_multi_select: co.allow_multi_select?, allow_no_selection: co.allow_no_selection?,
-       validation_regex: co.validation_regex ,suggested_pick_list_values: co.suggested_pick_list_values.map do |suggested|
+       validation_regex: co.validation_regex, suggested_pick_list_values: co.suggested_pick_list_values.map do |suggested|
          {suggested_value: suggested.value, suggested_description: suggested.description}
        end}
     end
