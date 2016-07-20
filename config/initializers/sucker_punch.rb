4edef06010f4cee5a4d9b2dd476e2b8da50deddc
cshupp <cshupp@gmail.com>
2016-07-20 11:30:56 -0400

    Extra logging for suckerpunch shutdown hooks.

--- config/initializers/sucker_punch.rb
+++ config/initializers/sucker_punch.rb
@@ -8,6 +8,7 @@ module ActiveJob
     class SuckerPunchAdapter
       class << self
         def enqueue_at(job, timestamp)
+          java.lang.Thread.currentThread.setName("Prisme Scheduled Job #{job}")
           @@scheduler ||= java.util.concurrent.Executors.newScheduledThreadPool(20)
           time_delay = [timestamp - Time.now.to_i, 0].max #java source code (for one impl) does this already, but to be safe...)
           @@scheduler.schedule(-> { JobWrapper.new.async.perform job.serialize }, time_delay, java.util.concurrent.TimeUnit::SECONDS);
@@ -18,9 +19,12 @@ module ActiveJob
             $log.info("Preparing to shutdown future scheduler")
             @@scheduler.shutdown
             bool = @@scheduler.awaitTermination(120, java.util.concurrent.TimeUnit::SECONDS)
-            @@scheduler.shutdownNow unless bool
+            unless bool
+              naughty_tasks = @@scheduler.shutdownNow.to_a
+              $log.warn("The scheduler had to be stopped via shutdownNow.")
+              $log.warn("#{naughty_tasks}")
+            end
             $log.info("Scheduler stopped!")
-            $log.warn("But the scheduler had to be stopped via shutdownNow") unless bool
           rescue => ex
             $log.error("I could not shut down the scheduler for active job. #{ex}")
             $log.error(ex.backtrace.join("\n"))
