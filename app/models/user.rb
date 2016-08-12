15321ebdba594ecb89af85db0f25ec1f8e65047a
gpbowman-git <gpbowman.git@gmail.com>
2016-08-12 17:42:25 -0400

    added sorting of initial listing; logic to not allow an admin to delete the last super_user; refactored user and ssoi_user to use PrismeUserConcern; added migration for adding the admin_role_check column to the user model/table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,9 @@
 class User < ActiveRecord::Base
+  include PrismeUserConcern
   before_save :ensure_read_only
-
-  rolify
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  # interface methods shared between user.rb and ssoi_user.rb
-  def user_name
-    email
-  end
-
-  def user_row_id
-    "#{id}_false"
-  end
-
-  private
-  def ensure_read_only
-    self.add_role(Roles::READ_ONLY)
-  end
 end
