a0e7618f2b8d041d3fdb19d639322afb546ddc43
cshupp <cshupp@gmail.com>
2016-08-15 12:40:07 -0400

    db builder back end, front end coming...

--- app/jobs/terminology_database_builder_job.rb
+++ app/jobs/terminology_database_builder_job.rb
@@ -22,20 +22,39 @@ class TerminologyDatabaseBuilder < PrismeBaseJob
     result_hash[:classify] = classify
     result_hash[:ibdf_files] = ibdf_files
     result_hash[:metadata_version] = metadata_version
-    result = ''
+    tag_name = ''
     begin
       $log.debug("About to create a DB CONFIG.")
-      result = IsaacDBConfigurationCreator::create_db_configuration(name: db_name, version: db_version, description: db_description,
+      tag_name = IsaacDBConfigurationCreator::create_db_configuration(name: db_name, version: db_version, description: db_description,
                                                                     result_classifier: artifact_classifier, classify_bool: classify,
                                                                     ibdf_files: ibdf_files, metadata_version: metadata_version,
                                                                     git_url: git_url, git_user: git_user, git_password: git_pass)
-      $log.debug("DB CONFIG=#{result}")
+      # these locals are used in erb call below! do not remove!
+      development = Rails.env.development?
+      git_failure = nil
+      # file to render
+      props = Service.get_build_server_props
+      j_xml = PrismeService::JENKINS_XML
+      url = props[PrismeService::JENKINS_ROOT]
+      user = props[PrismeService::JENKINS_USER]
+      password = props[PrismeService::JENKINS_PWD]
+
+      # nexus url - these variables are in the local binding for the erb - DO NOT REMOVE!
+      nexus_props = Service.get_artifactory_props
+      nexus_publication_url = nexus_props[PrismeService::NEXUS_PUBLICATION_URL]
+
+      # you MUST pass binding in order to have the erb process local variables
+      @job_xml = ERB.new(File.open(j_xml, 'r') { |file| file.read }).result(binding)
+      t_s = Time.now.strftime('%Y_%m_%dT%H_%M_%S')
+      job = JenkinsStartBuild.perform_later("#{JenkinsStartBuild::PRISME_NAME_PREFIX}DB_BUILDER_#{t_s}", @job_xml, url, user, password, track_child_job)
+      $log.debug("Jenkins start build called to build a db! #{job}")
+      $log.debug("DB CONFIG=#{tag_name}")
     rescue => ex
-      result = ex.message
+      tag_name = ex.message
       $log.error("DB CONFIG failed. #{ex}")
       raise ex
     ensure
-      save_result(result, result_hash)
+      save_result(tag_name, result_hash)
     end
 
   end
