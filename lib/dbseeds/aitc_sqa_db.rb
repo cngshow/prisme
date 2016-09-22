18c8289be4de5be4ee3feaf79f20c7ac51b15384
root <root@faxdevvpcsvr02.mantech.com>
2016-09-22 16:35:53 -0400

    modified:   aitc_sqa_db.rb

--- lib/dbseeds/aitc_sqa_db.rb
+++ lib/dbseeds/aitc_sqa_db.rb
@@ -5,30 +5,30 @@ module SeedData
           props: [
               {key: PrismeService::CARGO_REMOTE_USERNAME, value: 'devtest'},
               {key: PrismeService::CARGO_REMOTE_PASSWORD, value: '["jK\x90\xA1\x1Fk\x87\xB6\xAB\xA3\xC5\xE7~\xBA\x1AK", "k]\x95\xD8w\x15\xFE\xD3\xC7\xDC\xAC\x9E\x1C\xD0bG"]'},
-              {key: PrismeService::CARGO_REMOTE_URL, value: 'https://put-in-path-here:port/manager'}
+              {key: PrismeService::CARGO_REMOTE_URL, value: 'https://vaausdbsctt700.aac.va.gov:8080/manager'}
           ]
       },
       {
           service: {name: 'Nexus Artifactory', description: 'Nexus Artifactory', service_type: PrismeService::NEXUS},
           props: [
-              {key: PrismeService::NEXUS_REPOSITORY_URL, value: 'https://put-in-path-here:port/nexus/content/groups/everything/'},
-              {key: PrismeService::NEXUS_PUBLICATION_URL, value: 'https://put-in-path-here:port/nexus/content/repositories/termdata/'},
+              {key: PrismeService::NEXUS_REPOSITORY_URL, value: 'https://vaausappctt702.aac.va.gov:8443/nexus/content/groups/everything/'},
+              {key: PrismeService::NEXUS_PUBLICATION_URL, value: 'https://vaausappctt702.aac.va.gov:8443/nexus/content/repositories/termdata/'},
               {key: PrismeService::NEXUS_USER, value: 'devtest'},
-              {key: PrismeService::NEXUS_PWD, value: '["jK\x90\xA1\x1Fk\x87\xD7\xC3\xD8\xA8\x9A\x18\xD4fC"]'}
+              {key: PrismeService::NEXUS_PWD, value: '["jK\x90\xA1\x1Fk\x87\xB6\xAB\xA3\xC5\xE7~\xBA\x1AK", "k]\x95\xD8w\x15\xFE\xD3\xC7\xDC\xAC\x9E\x1C\xD0bG"]'}
           ]
       },
       {
           service: {name: 'GIT', description: 'GIT Version Control', service_type: PrismeService::GIT},
           props: [
-              {key: PrismeService::GIT_REPOSITORY_URL, value: 'https://put-in-path-here:port/git/r/repo_name.git'},
+              {key: PrismeService::GIT_REPOSITORY_URL, value: 'https://vaausdbsctt700.aac.va.gov:8080/git/r/repo_name.git'},
               {key: PrismeService::GIT_USER, value: 'devtest'},
-              {key: PrismeService::GIT_PWD, value: '["jK\x90\xA1\x1Fk\x87\xD7\xC3\xD8\xA8\x9A\x18\xD4fC"]'}
+              {key: PrismeService::GIT_PWD, value: '["jK\x90\xA1\x1Fk\x87\xB6\xAB\xA3\xC5\xE7~\xBA\x1AK", "k]\x95\xD8w\x15\xFE\xD3\xC7\xDC\xAC\x9E\x1C\xD0bG"]'}
           ]
       },
       {
           service: {name: 'Jenkins Build Server', description: 'Jenkins Build Server', service_type: PrismeService::JENKINS},
           props: [
-              {key: PrismeService::JENKINS_ROOT, value: 'https://put-in-path-here:port'},
+              {key: PrismeService::JENKINS_ROOT, value: 'https://vaausappctt702.aac.va.gov:8080'},
               {key: PrismeService::JENKINS_USER, value: 'devtest'},
               {key: PrismeService::JENKINS_PWD, value: '["jK\x90\xA1\x1Fk\x87\xB6\xAB\xA3\xC5\xE7~\xBA\x1AK", "k]\x95\xD8w\x15\xFE\xD3\xC7\xDC\xAC\x9E\x1C\xD0bG"]'}
           ]
