53c146f7b803a3d2bf10811b19590b35f3947c74
cshupp <cshupp@gmail.com>
2016-09-27 14:29:01 -0400

    Start of roles API in prisme

--- app/models/ssoi_user.rb
+++ app/models/ssoi_user.rb
@@ -14,4 +14,13 @@ class SsoiUser < ActiveRecord::Base
   def self.fetch_user(ssoi_user_name)
     SsoiUser.where(ssoi_user_name: ssoi_user_name).first
   end
+
+  def self.user_and_roles(ssoi_user_name)
+    roles = []
+    user = fetch_user(ssoi_user_name)
+    if user
+      roles = user.roles.to_a
+    end
+    {user: user, roles: roles}
+  end
 end
