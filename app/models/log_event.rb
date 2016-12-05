c8306c79b88c85ec4dc8ec707ed7b18ddcc941dd
cshupp <cshupp@gmail.com>
2016-12-05 10:33:08 -0500

    LogEvent cleanup code.

--- app/models/log_event.rb
+++ app/models/log_event.rb
@@ -9,7 +9,23 @@ class LogEvent < ActiveRecord::Base
     return if LEVELS.values.include?(level)
     errors.add :level_error, "Invalid level. The level must be an integer corresponding to #{LEVELS.map do |k,v| [k, v.to_s] end}"
   end
+
+  def self.cleanup(older_than_in_days = 90)
+    begin
+      older_than_in_days = older_than_in_days.days.ago
+      $log.info("Cleaning up all records in log event table older than #{older_than_in_days} days.")
+      cnt = LogEvent.where('created_at < ?', *[older_than_in_days]).delete_all
+      $log.info("#{cnt} log events deleted.")
+    rescue => ex
+      $log.warn("Cleanup in log event table failed. #{ex}")
+      $log.warn(ex.backtrace.join("\n"))
+    end
+  end
+
 end
 
+
+
 #copy pastables:
+# load('./app/models/log_event.rb')
 # LogEvent.destroy_all
