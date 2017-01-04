ad53fabddb7496b42dbb2622469a442948545706
cshupp <cshupp@gmail.com>
2017-01-04 12:46:29 -0500

    converting initial load of site data to be in synch with Greg's migrations.  This way one row of json is passable to an ActiveRecord constructor.

--- config/tds/tds_converter.rb
+++ config/tds/tds_converter.rb
@@ -17,8 +17,8 @@ end
 
 sites = []
 hash[:SITES].keys.each do |index|
-  sites << {'VA_SITE_ID' => hash[:SITES][index][:VA_SITE_ID].to_s,'NAME' => hash[:SITES][index][:NAME].to_s, 'SITE_TYPE' => hash[:SITES][index][:TYPE].to_s, 'MESSAGE_TYPE' => hash[:SITES][index][:MESSAGE_TYPE].to_s}
-  # sites << {'VA_SITE_ID' => hash[:SITES][index][:VA_SITE_ID].to_s,'NAME' => hash[:SITES][index][:NAME].to_s, 'SITE_TYPE' => hash[:SITES][index][:TYPE].to_s}
+ # sites << {'va_site_id' => hash[:SITES][index][:VA_SITE_ID].to_s,'name' => hash[:SITES][index][:NAME].to_s, 'site_type' => hash[:SITES][index][:TYPE].to_s, 'message_type' => hash[:SITES][index][:MESSAGE_TYPE].to_s}
+  sites << {'va_site_id' => hash[:SITES][index][:VA_SITE_ID].to_s,'name' => hash[:SITES][index][:NAME].to_s, 'site_type' => hash[:SITES][index][:TYPE].to_s}
 end
 #to yaml is only available in rails, run in console not scratch.
-File.open('C:\work\va-ctt\rails\rails_prisme\config\tds\site_data_message_type.yml','w') do |f| f.write sites.to_yaml end
+File.open('C:\work\va-ctt\rails\rails_prisme\config\tds\site_data.yml','w') do |f| f.write sites.to_yaml end
