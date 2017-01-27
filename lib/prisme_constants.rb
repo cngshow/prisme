10af8e121f16ddfe9d34edba00c4d1a43658a379
cshupp <cshupp@gmail.com>
2017-01-27 10:27:44 -0500

    hl7 environment constant support added.

--- lib/prisme_constants.rb
+++ lib/prisme_constants.rb
@@ -4,4 +4,12 @@ module PrismeConstants
     TERMINOLOGY_DB_BUILDER = 'terminology_db_builder_tag'
     APP_DEPLOYER = 'app_deployer'
   end
+  module ENVIRONMENT
+    DEV = :DEV
+    SQA = :INTEGRATION
+    INTEGRATION = SQA
+    PRE_PROD = :'PRE-PROD'
+    PROD = :PROD
+    DEV_BOX = :DEV_BOX
+  end
 end
