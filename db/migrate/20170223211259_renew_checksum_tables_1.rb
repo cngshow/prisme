c1af42ca890def779781d8048e00090d5913ce90
cshupp <cshupp@gmail.com>
2017-02-23 17:06:16 -0500

    update to checksum tables.

---
+++ db/migrate/20170223211259_renew_checksum_tables_1.rb
@@ -0,0 +1,31 @@
+class RenewChecksumTables1 < ActiveRecord::Migration
+  def change
+    ActiveRecord::Migrator.migrate('db/migrate', 20170130164539)
+
+    create_table :checksum_requests do |t|
+      t.string :username, null: false
+      t.string :domain, null: false
+      t.timestamps null: false
+    end
+
+    add_index(:checksum_requests, [:domain])
+
+    create_table :checksum_details do |t|
+      t.references :va_site, index: true, null: false
+      t.references :checksum_request, index: true, null: false
+      t.references :checksum_detail, index: true, null: true
+      t.string :subset
+      t.string :checksum
+      t.string :failure_message
+      t.text :hl7_message
+      t.string :version
+      t.string :status, null: false
+      t.date :start_time
+      t.date :finish_time
+      t.timestamps null: false
+    end
+
+  end
+end
+
+
