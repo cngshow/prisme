57c989fd5a2e5ef3a818a895f57d4a67777c7533
gpbowman-git <gpbowman.git@gmail.com>
2017-02-27 12:56:47 -0500

    testing new migrations

--- db/migrate/20170223211259_renew_checksum_tables_1.rb
+++ db/migrate/20170223211259_renew_checksum_tables_1.rb
@@ -1,6 +1,8 @@
 class RenewChecksumTables1 < ActiveRecord::Migration
   def change
-    ActiveRecord::Migrator.migrate('db/migrate', 20170130164539)
+    drop_table :checksum_details rescue false
+    drop_table :checksum_requests rescue false
+    ActiveRecord::Base.connection.execute "delete from SCHEMA_MIGRATIONS where version in ('20170209192046','20170209192335')" rescue false
 
     create_table :checksum_requests do |t|
       t.string :username, null: false
