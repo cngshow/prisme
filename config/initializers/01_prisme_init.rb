3dea715bd7199857a3cc738eee9f155e5ea39981
cshupp <cshupp@gmail.com>
2017-02-28 10:17:01 -0500

    Lost some code in a merge...

--- config/initializers/01_prisme_init.rb
+++ config/initializers/01_prisme_init.rb
@@ -110,7 +110,7 @@ PRISME_ENVIRONMENT = PrismeUtilities.aitc_environment.fetch(Socket.gethostname) 
 $log.always { PrismeLogEvent.notify(PrismeLogEvent::LIFECYCLE_TAG, "#{Rails.application.class.parent_name} coming up!  The version is #{PRISME_VERSION}") }
 PRISME_NAME = $PROPS['PRISME.application_name']
 KOMET_NAME = $PROPS['PRISME.komet_name']
-HL7Messaging.initialize
+HL7Messaging.init_messaging_engine
 #started = HL7Messaging.init_messaging_engine
 #$log.info("Messaging engine started?: #{started}")
 # ensure super_user and admin for cboden for demo
