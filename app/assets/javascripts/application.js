c28eb150924b2ae7bcb3868b911a102f35417ba4
cshupp <cshupp@gmail.com>
2017-03-13 15:26:01 -0400

    testing discovery csv on IE.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -115,3 +115,18 @@ function init_select2() {
         });
     });
 }
+//http://stackoverflow.com/questions/2897619/using-html5-javascript-to-generate-and-save-a-file
+function string_to_file(filename, text) {
+    var pom = document.createElement('a');
+    pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    pom.setAttribute('download', filename);
+
+    if (document.createEvent) {
+        var event = document.createEvent('MouseEvents');
+        event.initEvent('click', true, true);
+        pom.dispatchEvent(event);
+    }
+    else {
+        pom.click();
+    }
+}
