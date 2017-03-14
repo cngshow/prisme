e2512de79193506de870dec9e010b798ddc3f4ed
cshupp <cshupp@gmail.com>
2017-03-14 10:27:34 -0400

    Download csv of previous vs current impl.

--- lib/hl7_message.rb
+++ lib/hl7_message.rb
@@ -292,11 +292,11 @@ module HL7Messaging
     end
 
     def mock
-      if (rand(5).eql? 0) #Checking this in will randomly break the build.  Uncomment as needed.
-        @detail.status = FAILED
-        @detail.failure_message = "I am a loser!!!"*rand(10)
-        return
-      end
+      # if (rand(5).eql? 0) #Checking this in will randomly break the build.  Uncomment as needed.
+      #   @detail.status = FAILED
+      #   @detail.failure_message = "I am a loser!!!"*rand(10)
+      #   return
+      # end
       if (@detail.class.eql? ChecksumDetail)
         file = Tempfile.new('checksum_simulator')
         file.write([*('a'..'z'), *('0'..'9')].shuffle[0, 36].join)
