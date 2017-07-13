8b5fdaa2f7c26735115e43c191d77c9947db9be6
cshupp <cshupp@gmail.com>
2017-07-13 16:44:15 -0400

    Save not working...

--- app/models/user_role_assoc.rb
+++ app/models/user_role_assoc.rb
@@ -3,15 +3,27 @@ class UserRoleAssoc < ActiveRecord::Base
   belongs_to :user
 
   # belongs_to :role
-  def fetch_role_metadata
-    JSON.parse(role_metadata)
+  def fetch_metadata
+    JSON.parse(role_metadata).to_s
   end
 
-  def write_metadata(**hash)
+  def write_metadata?(**hash)
     role_metadata = hash.to_json
-    update!
+    puts "r is #{role_metadata}"
+    update(role_metadata: role_metadata)
+    #save
+  end
+
+  def write_metadata!(**hash)
+    role_metadata = hash.to_json
+    save!
   end
 
   def has_isaac_uuid(uuid)
   end
 end
+
+=begin
+a = User.first.user_role_assocs.first
+a.write_metadata?(foo: 'fff')
+=end
