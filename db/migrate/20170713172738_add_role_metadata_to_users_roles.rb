68dde5d300215eb43388956c7f8a4a7e3ec46962
cshupp <cshupp@gmail.com>
2017-07-14 12:06:15 -0400

    Fix migration for H2

--- db/migrate/20170713172738_add_role_metadata_to_users_roles.rb
+++ db/migrate/20170713172738_add_role_metadata_to_users_roles.rb
@@ -1,27 +1,25 @@
 class AddRoleMetadataToUsersRoles < ActiveRecord::Migration
   def self.up
-    if $database.eql?(RailsPrisme::ORACLE)
-      # store off the existing data and truncate the table so we can add the id as the primary key
-      data = UserRoleAssoc.all.as_json
-      execute %q{TRUNCATE TABLE users_roles}
+    # store off the existing data and truncate the table so we can add the id as the primary key
+    data = UserRoleAssoc.all.as_json
+    execute %q{TRUNCATE TABLE users_roles}
 
-      # add the role_metadata and id columns
-      add_column :users_roles, :role_metadata, :text, required: false
-      add_column :users_roles, :id, :primary_key
+    # add the role_metadata and id columns
+    add_column :users_roles, :role_metadata, :text, required: false
+    add_column :users_roles, :id, :primary_key
 
-      # reload the table using the data above
-      idx = 0
-      data.each do |row|
-        idx = idx + 1
-        execute "insert into users_roles (id, user_id, role_id) values (#{idx},#{row['user_id']},#{row['role_id']})"
-      end
+    # reload the table using the data above
+    idx = 0
+    data.each do |row|
+      idx = idx + 1
+      execute "insert into users_roles (id, user_id, role_id) values (#{idx},#{row['user_id']},#{row['role_id']})"
     end
   end
 
   def self.down
+    remove_column :users_roles, :role_metadata
+    remove_column :users_roles, :id
     if $database.eql?(RailsPrisme::ORACLE)
-      remove_column :users_roles, :role_metadata
-      remove_column :users_roles, :id
       execute %q{DROP SEQUENCE USERS_ROLES_SEQ}
     end
   end
