6e5abe976209855754d87794594f482b0ab8114b
gpbowman-git <gpbowman.git@gmail.com>
2017-07-14 11:01:51 -0400

    added migration of devise user role data preserving the data and adding the necessary id primary key and metadata column

--- app/models/user_role_assoc.rb
+++ app/models/user_role_assoc.rb
@@ -1,5 +1,5 @@
 class UserRoleAssoc < ActiveRecord::Base
-  self.table_name = "users_roles"
+  self.table_name = 'users_roles'
   belongs_to :user
 
   # belongs_to :role
--- db/migrate/20170713172738_add_role_metadata_to_users_roles.rb
+++ db/migrate/20170713172738_add_role_metadata_to_users_roles.rb
@@ -1,5 +1,28 @@
 class AddRoleMetadataToUsersRoles < ActiveRecord::Migration
-  def change
-    add_column :users_roles, :role_metadata, :text, required: false
+  def self.up
+    if $database.eql?(RailsPrisme::ORACLE)
+      # store off the existing data and truncate the table so we can add the id as the primary key
+      data = UserRoleAssoc.all.as_json
+      execute %q{TRUNCATE TABLE users_roles}
+
+      # add the role_metadata and id columns
+      add_column :users_roles, :role_metadata, :text, required: false
+      add_column :users_roles, :id, :primary_key
+
+      # reload the table using the data above
+      idx = 0
+      data.each do |row|
+        idx = idx + 1
+        execute "insert into users_roles (id, user_id, role_id) values (#{idx},#{row['user_id']},#{row['role_id']})"
+      end
+    end
+  end
+
+  def self.down
+    if $database.eql?(RailsPrisme::ORACLE)
+      remove_column :users_roles, :role_metadata
+      remove_column :users_roles, :id
+      execute %q{DROP SEQUENCE USERS_ROLES_SEQ}
+    end
   end
 end
