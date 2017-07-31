c37be81d82721b5735b69303bdb4d2105101dbf4
cshupp <cshupp@gmail.com>
2017-07-31 10:50:58 -0400

    Fix prisme build to ignore non user roles.

--- test/integration/roles_test.rb
+++ test/integration/roles_test.rb
@@ -8,11 +8,15 @@ java_import 'gov.vha.isaac.ochre.api.PrismeRole' do |p,c|
   'JPR'
 end
 
+java_import 'gov.vha.isaac.ochre.api.PrismeRoleType' do |p,c|
+  'JPRT' #Stands for Java Prisme Role Type
+end
+
 class RolesTest < ActionDispatch::IntegrationTest
   include TestUser
 
   test 'all_roles_available' do
-    all_java_roles = JPR.values.map do |role| role.name.to_sym end.sort
+    all_java_roles = JPR.values.select do |role| role.getType != JPRT::NON_USER  end.map do |role| role.name.to_sym end.sort
     role_roles = (Roles.constants & all_java_roles).sort
     assert(role_roles.eql?(all_java_roles), "Expected all the roles defined in java to be referenced in Ruby! Java has #{all_java_roles}, ruby only has #{role_roles}.  Missing #{all_java_roles - role_roles} ")
   end
