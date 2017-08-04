8f4fe0f0c72130ee55fefe5864c6c4eca76b1b71
cshupp <cshupp@gmail.com>
2017-08-04 18:14:01 -0400

    Sigh, build no worky

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,70 +3,75 @@
 // https://github.com/shakacode/react-webpack-rails-tutorial/tree/master/client
 
 const webpack = require('webpack');
-const pathLib = require('path');
+const { resolve } = require('path');
 
 const ManifestPlugin = require('webpack-manifest-plugin');
 const webpackConfigLoader = require('react-on-rails/webpackConfigLoader');
 
-const devBuild = process.env.NODE_ENV !== 'production';
-
+const configPath = resolve('..', 'config');
+const { devBuild, manifest, webpackOutputPath, webpackPublicOutputDir } =
+    webpackConfigLoader(configPath);
 
 const config = {
 
-  entry: {
-    'webpack-bundle': [
-      'es5-shim/es5-shim',
-      'es5-shim/es5-sham',
-      'babel-polyfill',
-      './app/bundles/LogEvents/startup/registration',
+    context: resolve(__dirname),
+
+    entry: {
+        'webpack-bundle': [
+            'es5-shim/es5-shim',
+            'es5-shim/es5-sham',
+            'babel-polyfill',
+            './app/bundles/HelloWorld/startup/registration',
+        ],
+    },
+
+    output: {
+        // Name comes from the entry section.
+        filename: '[name]-[hash].js',
+
+        // Leading slash is necessary
+        publicPath: `/${webpackPublicOutputDir}`,
+        path: webpackOutputPath,
+    },
+
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+
+    plugins: [
+        new webpack.EnvironmentPlugin({
+            NODE_ENV: 'development', // use 'development' unless process.env.NODE_ENV is defined
+            DEBUG: false,
+        }),
+        new ManifestPlugin({ fileName: manifest, writeToFileEmit: true }),
     ],
-  },
 
-  output: {
-    // Name comes from the entry section.
-    filename: '[name]-[hash].js',
-
-    // Leading slash is necessary
-    path: pathLib.resolve(__dirname, '../app/assets/webpack'),
-  },
-
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-
-  plugins: [
-    new webpack.EnvironmentPlugin({
-      NODE_ENV: 'development', // use 'development' unless process.env.NODE_ENV is defined
-      DEBUG: false,
-    }),
-  ],
-
-  module: {
-    rules: [
-      {
-        test: require.resolve('react'),
-        use: {
-          loader: 'imports-loader',
-          options: {
-            shim: 'es5-shim/es5-shim',
-            sham: 'es5-shim/es5-sham',
-          },
-        },
-      },
-      {
-        test: /\.jsx?$/,
-        use: 'babel-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
+    module: {
+        rules: [
+            {
+                test: require.resolve('react'),
+                use: {
+                    loader: 'imports-loader',
+                    options: {
+                        shim: 'es5-shim/es5-shim',
+                        sham: 'es5-shim/es5-sham',
+                    },
+                },
+            },
+            {
+                test: /\.jsx?$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
 };
 
 module.exports = config;
 
 if (devBuild) {
-  console.log('Webpack dev build for Rails'); // eslint-disable-line no-console
-  module.exports.devtool = 'eval-source-map';
+    console.log('Webpack dev build for Rails'); // eslint-disable-line no-console
+    module.exports.devtool = 'eval-source-map';
 } else {
-  console.log('Webpack production build for Rails'); // eslint-disable-line no-console
+    console.log('Webpack production build for Rails'); // eslint-disable-line no-console
 }
