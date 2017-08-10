6d9339b65b1cfaeb248734573af9863471402000
cshupp <cshupp@gmail.com>
2017-08-10 15:25:34 -0400

    role cleanup

--- app/models/role.rb
+++ app/models/role.rb
@@ -4,11 +4,31 @@ class Role < ActiveRecord::Base
 
   belongs_to :resource,
              :polymorphic => true
-             # :optional => true
+  # :optional => true
 
   validates :resource_type,
-            :inclusion => { :in => Rolify.resource_types },
+            :inclusion => {:in => Rolify.resource_types},
             :allow_nil => true
 
   scopify
+
+  BAD_ROLE_SQL = %Q{select a.id from roles a where not exists (select * from roles b where a.id = b.id and b.name in (#{Roles::ALL_ROLES.map do |e|
+    '\''<< e << '\''
+  end.join(',')}))}
+
+
+  def self.cleanup_removed_roles
+    begin
+      Role.find_by_sql(Role::BAD_ROLE_SQL).each do |bad_role|
+        $log.info("The following role is now defunct - #{Role.find(bad_role.id).name}, cleaning up...")
+        UserRoleAssoc.destroy_all(role_id: bad_role.id)
+        SsoiUserRoleAssoc.destroy_all(role_id: bad_role.id)
+        Role.destroy(bad_role.id)
+    end
+    rescue => ex
+      $log.error("Failure to cleanup defunct roles! #{ex}")
+      $log.error(ex.backtrace.join("\n"))
+    end
+  end
+
 end
